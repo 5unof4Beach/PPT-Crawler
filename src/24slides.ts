dc20b3028d548e218ba61263a55f3d09794f1fe4
Duc Bui <duc.bui@starack.net>
2023-07-06 15:42:48 +0700

    feat(PPT-01): add download link retriever

--- src/24slides.ts
+++ src/24slides.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import axios from "axios";
 import * as cheerio from "cheerio";
 import puppeteer from "puppeteer";
@@ -8,13 +9,13 @@ const linkList: string[] = [];
 const dlinkList: string[] = [];
 
 const getAllLink = async () => {
-  //   let i = 0;
+  // let i = 0;
   let nextPage =
     "https://24slides.com/templates/paginate/featured?page=2&offset=12";
   do {
     try {
-      //   i = i + 1;
-      console.log(`Retriving data from ${nextPage}`)
+      // i = i + 1;
+      console.log(`Retriving data from ${nextPage}`);
       const response = await axios.get(nextPage);
       const { html, pagination_next_page_url } = response.data;
       nextPage = pagination_next_page_url;
@@ -37,18 +38,46 @@ const retrieveSlideLinkFromHtml = async (html: string) => {
 };
 
 const scrapeAllSlideDownloadLink = async (slideLinks: string[]) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(baseURL);
-  await page.setViewport({ width: 1080, height: 1024 });
+  await page.setViewport({ width: 1200, height: 800 });
   await page.waitForSelector(".openLoginCard");
   await page.click(".openLoginCard");
-  slideLinks.map((url) => {});
+  const $email = await page.waitForSelector("#loginEmail");
+  const $password = await page.waitForSelector("#loginPassword");
+  const $loginBtn = await page.waitForSelector(".login-btn");
+
+  await $email?.type(process.env.TF_SLIDE_EMAIL ?? "");
+  await $password?.type(process.env.TF_SLIDE_PASSWORD ?? "");
+  await Promise.all([
+    $loginBtn?.click(),
+    page.waitForNavigation({ waitUntil: "networkidle0" }),
+  ]);
+
+  for (let i = 0; i < slideLinks.length; i++) {
+    const url = slideLinks[i];
+    await page.goto(url, {
+      waitUntil: "domcontentloaded",
+    });
+    const content = await page.content();
+    try {
+      const $ = cheerio.load(content);
+      const downloadLink = $("a.btn-download").attr("href") ?? "";
+      console.log("Download link retrieved " + downloadLink);
+      dlinkList.push(downloadLink);
+    } catch (error) {
+      console.error(`${dlinkList.length} download links retrieved`);
+      console.error(error);
+    }
+  }
+
+  await browser.close();
 };
 
 (async () => {
   await getAllLink();
-  console.log(linkList, `Total: ${linkList.length}`);
-  // await downloadLinks(linkList)
+  await scrapeAllSlideDownloadLink(linkList);
+  console.log(dlinkList, `Total: ${dlinkList.length}`);
   // await downloadFiles(dlinkList)
 })();
