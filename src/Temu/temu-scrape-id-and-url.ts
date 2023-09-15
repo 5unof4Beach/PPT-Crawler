b27066ad35e1329facd8f0b448c0491d6ed167f9
Duc Bui <duc.bui@savvycomsoftware.com>
2023-09-16 01:34:59 +0700

    Update

--- src/Temu/temu-scrape-id-and-url.ts
+++ src/Temu/temu-scrape-id-and-url.ts
@@ -1,4 +1,4 @@
-import { initiateNewBrowser } from "./utils";
+import { initiateNewBrowser, randomNumber, wait } from "./utils";
 import { GeneralInfo } from "./types";
 import axios from "axios";
 import * as cheerio from "cheerio";
@@ -37,83 +37,34 @@ const retrieveSlideLinkFromHtml = async (html: string) => {
   }
 };
 
-const scrape = async (slideLinks: string[]) => {
-  const total = slideLinks.length;
+const scrape = async () => {
   let { page, browser } = await initiateNewBrowser();
-  // await login(page);
-
-  // await scrapeIdsAndProductUrls(page);
-
-  // for (let i = 0; i < total; i++) {
-  //   try {
-  //     const url = slideLinks[i];
-  //     await page
-  //       .goto(url, {
-  //         waitUntil: "domcontentloaded",
-  //       })
-  //       .catch((e) => {
-  //         //throw error case of timeout
-  //         console.log(`${dlinkList.length} download links retrieved`);
-  //         throw e;
-  //       });
-  //     const content = await page.content();
-  //     const $ = cheerio.load(content);
-  //     const downloadLink = $("a.btn-download").attr("href") ?? "";
-  //     const category =
-  //       $("ol.breadcrumb-menu > li:nth-child(2) span").text() ?? "";
-  //     const name = $("ol.breadcrumb-menu > li:nth-child(3) span").text() ?? "";
-  //     console.log(
-  //       `Download link retrieved ${downloadLink} | total: ${
-  //         dlinkList.length + 1
-  //       }/${total}`
-  //     );
-  //     if (downloadLink) {
-  //       dlinkList.push({
-  //         category,
-  //         name: name.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, ""),
-  //         id: slideLinks[i],
-  //         productUrl: downloadLink,
-  //       });
-  //     } else {
-  //       --i;
-  //     }
-  //   } catch (error) {
-  //     // Create new browser after current browser got blocked
-  //     console.log(
-  //       "---------------------- Request timeout creating new browser ----------------------"
-  //     );
-  //     browser.disconnect();
-  //     const newBrowser = await initiateNewBrowser();
-  //     page = newBrowser.page;
-  //     browser = newBrowser.browser;
-  //     await login(page);
-  //     --i;
-  //   }
-  // }
-  // await browser.close();
+  await scrapeIdsAndProductUrls(page);
 };
 
 const removeScrapedElements = async (page: any) => {
   await page.evaluate(() => {
-    const elementsToRemove = document.querySelectorAll('.crawled');
+    const elementsToRemove = document.querySelectorAll(".crawled");
     elementsToRemove.forEach((element: any) => element.remove());
   });
 };
 
 const scrapeIdsAndProductUrls = async (page: any) => {
   const baseURL =
-    "https://www.temu.com/home-kitchen-o3-36.html?opt_level=1&title=Home%20%26%20Kitchen&_x_enter_scene_type=cate_tab&leaf_type=son&show_search_type=3&refer_page_el_sn=200053&refer_page_name=home&refer_page_id=10005_1694697438760_2ay7esmmo1&refer_page_sn=10005&_x_sessn_id=u8wkv0rnhx&filter_items=3%3A1";
+    "https://www.temu.com/home-kitchen-o3-36.html?opt_level=1&title=Home%20%26%20Kitchen&_x_enter_scene_type=cate_tab&leaf_type=son&show_search_type=3&_x_sessn_id=sjj71r67x1&refer_page_name=home&refer_page_id=10005_1694789023211_trtu3mn754&refer_page_sn=10005&filter_items=3%3A1";
   await page.goto(baseURL, {
     waitUntil: "domcontentloaded",
   });
 
-  let i = 2;
+  let i = 20;
   while (i !== 0) {
+    await page.mouse.wheel({ deltaY: 500 });
     const showMoreBtn = await page.waitForSelector("._2U9ov4XG", {
       visible: true,
       timeout: 5000,
     });
     const productCards = await page.$$("._3GizL2ou");
+    while (productCards.length === 0) await showMoreBtn.click();
     for (const el of productCards) {
       await el.evaluate((el: any) => {
         el.classList.remove("_3GizL2ou");
@@ -122,9 +73,9 @@ const scrapeIdsAndProductUrls = async (page: any) => {
     }
 
     await removeScrapedElements(page);
-    console.log('done')
+    console.log("done " + i + " amount of items: " + productCards.length);
     await showMoreBtn.click();
-
+    await wait(randomNumber(20000, 25000));
     i--;
   }
 };
@@ -175,7 +126,7 @@ const exportJSON = (templates: GeneralInfo[]) => {
 };
 
 export const scrapeTemplates = async () => {
-  await scrape(linkList);
+  await scrape();
   // console.log(`Download links scraping done, total: ${dlinkList.length}`);
   // exportCSV(dlinkList);
   // exportJSON(dlinkList);
--- src/Temu/utils.ts
+++ src/Temu/utils.ts
@@ -23,43 +23,68 @@ export const login = async (page: any) => {
   let successful = false;
   // Navigate to the website with the Google login popup
   await page.goto("https://www.temu.com/login.html?login_scene=8", {
-    waitUntil: "domcontentloaded",
+    waitUntil: "networkidle0",
   });
 
-  // Wait for the login button to appear
-  await page.waitForSelector(".container-ja9M1");
+  await page.type("#user-account", "ducgrand2001@gmail.com");
+  const continueBtn = await page.$$(".loginBtn-3-2P7");
+  await continueBtn[0].click();
+  await page.waitForSelector("#pwdInputInLoginDialog");
+  await page.type("#pwdInputInLoginDialog", "hello123");
+  const signInBtn = await page.$$(".loginBtn-3-2P7");
+  await signInBtn[0].click();
+  page
+    .waitForNavigation({ waitUntil: "domcontentloaded" })
+    .catch((error: any) => {
+      console.log(error);
+    });
 
-  // Wait for the login button to appear
-  const buttons = await page.$$(".container-ja9M1");
-
-  // Click on the login button to open the Google login popup
-  await buttons[0].click();
-
-  // Wait for the Google login popup to appear
-  await page.waitForSelector('iframe[src*="accounts.google.com"]');
-
-  // Get the Google login iframe
-  const googleLoginFrame = page
-    .frames()
-    .find((frame: any) => frame.url().includes("accounts.google.com"));
-
-  // Interact with the elements in the Google login iframe
-  await googleLoginFrame.waitForSelector('input[type="email"]');
-  await googleLoginFrame.type(
-    'input[type="email"]',
-    "duc.bui.reserved@gmail.com"
-  );
-  await googleLoginFrame.click("#identifierNext");
-
-  await googleLoginFrame.waitForSelector('input[type="password"]');
-  await googleLoginFrame.type('input[type="password"]', "Buiminhduc2001");
-  await googleLoginFrame.click("#passwordNext");
-
-  // Wait for the login process to complete (adjust this wait time based on your specific scenario)
-  await page.waitForTimeout(5000);
-
-  // Continue interacting with the website after logging in
-  // ...
-
-  // await browser.close();
+  await wait(2000);
 };
+
+export const wait = async (amount: number) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Waiting for 5s before sraping new content");
+      resolve("");
+    }, amount);
+  });
+};
+
+export function randomNumber(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+// Wait for the login button to appear
+// await page.waitForSelector(".container-ja9M1");
+
+// // Wait for the login button to appear
+// const buttons = await page.$(".container-ja9M1");
+
+// console.log(buttons[0])
+
+// // Click on the login button to open the Google login popup
+// await buttons[1].click();
+
+// // Wait for the Google login popup to appear
+// await page.waitForSelector('iframe[src*="accounts.google.com"]');
+
+// // Get the Google login iframe
+// const googleLoginFrame = page
+//   .frames()
+//   .find((frame: any) => frame.url().includes("accounts.google.com"));
+
+// // Interact with the elements in the Google login iframe
+// await googleLoginFrame.waitForSelector('input[type="email"]');
+// await googleLoginFrame.type(
+//   'input[type="email"]',
+//   "duc.bui.reserved@gmail.com"
+// );
+// await googleLoginFrame.click("#identifierNext");
+
+// await googleLoginFrame.waitForSelector('input[type="password"]');
+// await googleLoginFrame.type('input[type="password"]', "Buiminhduc2001");
+// await googleLoginFrame.click("#passwordNext");
+
+// // Wait for the login process to complete (adjust this wait time based on your specific scenario)
+// await page.waitForTimeout(5000);
